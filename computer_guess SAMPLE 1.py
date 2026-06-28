0cb660c5872fb1dc86e4982cb8d8c35181a3c1ad
Fatima <sabafatimasohu@gmail.com>
2026-06-29 00:53:49 +0500

    Refactor guessing game into a function
    Refactored the number guessing game to use a function that allows the computer to guess a number between 1 and x, with user feedback on guesses.

--- computer_guess SAMPLE 1.py
+++ computer_guess SAMPLE 1.py
@@ -1,18 +1,29 @@
 # SAMPLE 1 imple straight forward code for guessing number game
 
+# SAMPLE 1  Straightforward approach to computer guessing a number between 1 and x where computer guesses
+# randomly and user (us) provides feedback on whether the guess is too high, too low, or correctand
+# we aslo provide x the argument to the function computer_guess(x) as a range for the computer to guess from 1 to x.
+# The computer will keep guessing until it gets the correct number and will also count the number of attempts it took to guess correctly.
+
 import random
-comp = random.randint(1,100)
-attemps = 0
-while True:
-   user = int(input("Guess the Number : "))
-   attemps = attemps + 1
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    attempts = 0
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low  # could also be high b/c low = high
+            
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        attempts = attempts + 1
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
 
-   if(user == comp):
-           print(f"You guessed the right Number {comp}\nYour attemps were {attemps}")
-           break
+    print(f'Yay! The computer guessed your number, {guess}, correctly! under {attempts} attempts!')
 
-   elif(user>comp):
-        print("LOWER IT PLZ")
-
-   elif(user<comp):
-       print("HIGHER IT PLZ")
+computer_guess(100)
